25bfa7de897396f75c099019120fb8e2ee4bb172
Francesco Sacco <francesco215@live.it>
2018-12-05 16:07:27 +0100

    minor

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
 
 setuptools.setup(
       name="menzalib",
-      version="0.5.9",
+      version="0.5.10",
       author="Lettera, Francesco215",
       author_email="",
       description="Funzioni utili per lab3",
-      long_description=long_description,
-      long_description_content_type="text/markdown"
-      url="https://bitbucket.org/Lettera/menza-lib",
+      #long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://bitbucket.org/Lettera/menza-lib/src",
       packages=setuptools.find_packages(),
       )
