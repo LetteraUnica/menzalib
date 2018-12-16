430795b30107ca4d5a19b6bf43cbbad4d1b4a2f8
Lorenzo <lorenzocav97@gmail.com>
2018-12-16 15:06:46 +0100

    asd

--- library_build.sh
+++ library_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 -m pip install --user --upgrade setuptools wheel twine
+python3 -m pip install --user --upgrade setuptools wheel
 
 rm -r build dist
 python3 setup.py sdist bdist_wheel
--- upload_library.sh
+++ upload_library.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+python3 -m pip install --user --upgrade twine
 twine upload dist/*
